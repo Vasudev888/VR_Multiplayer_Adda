48945aa560d034b88bfc1e2e2c6985cd9f83d96c
vasuReinv <vasudev@reinvision.com>
2022-09-14 11:39:05 +0530

    multiplayer sync worked

---
+++ Assets/Scenes/Test.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    void Start()
+    {
+        Vector3 pos = transform.position;
+        pos.x = 10;
+    }
+
+
+}
